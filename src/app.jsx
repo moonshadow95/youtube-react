695a4ae3600b2fe60f67313e5f890b3b1d91b50d
Moonshadow <last_enemy@naver.com>
2021-04-10 15:21:55 +0900

    show video list with title by use youtube api

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCFAbwMROkRy-Te_xDQxRkG6d-yIDxNszU',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+    console.log('use effect');
+  }, []);
+  // []를 사용하여 마운트 되었을 때 만 실행
+  return <VideoList videos={videos} />;
 }
 
 export default App;
